8f93bb6cd5cdda97d746344343081386d0e12be2
Andrey <ivankolecnik19@gmail.com>
2025-10-13 22:35:15 +0300

    fix connect db new

--- src/config/db.ts
+++ src/config/db.ts
@@ -7,6 +7,8 @@ const pool: Pool = mysql.createPool({
   user: process.env.DB_PROD_USER!,
   password: process.env.DB_PROD_PASSWORD!,
   database: process.env.DB_PROD_NAME!,
+  port: Number(process.env.DB_PROD_PORT!),
+  ssl: { rejectUnauthorized: false }
 });
 
 export default pool;
