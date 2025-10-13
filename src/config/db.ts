9f0511361c20bebe77d565f1f4fa466a8e893e85
Andrey <ivankolecnik19@gmail.com>
2025-10-13 23:20:37 +0300

    fix connect db last

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,13 @@
+import 'dotenv/config';
 import mysql from "mysql2/promise";
 import type { Pool } from "mysql2/promise";
-import 'dotenv/config';
 
 const pool: Pool = mysql.createPool({
-  host: process.env.DB_PROD_HOST!,
-  user: process.env.DB_PROD_USER!,
-  password: process.env.DB_PROD_PASSWORD!,
-  database: process.env.DB_PROD_NAME!,
-  port: Number(process.env.DB_PROD_PORT!),
+  host: String(process.env.DB_PROD_HOST),
+  user: String(process.env.DB_PROD_USER),
+  password: String(process.env.DB_PROD_PASSWORD),
+  database: String(process.env.DB_PROD_NAME),
+  port: Number(process.env.DB_PROD_PORT),
   ssl: { rejectUnauthorized: false }
 });
 
